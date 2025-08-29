50f9f991a6ad397f330233f191c1a3a0ca0d2ebc
Deepali <joysingal247@gmail.com>
2025-08-29 11:32:02 +0530

    hide sensitive information

--- src/Firebase/FirebaseConfig.jsx
+++ src/Firebase/FirebaseConfig.jsx
@@ -3,14 +3,16 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHKS1vaaC7uJ8maQnGRALMngvnR3KJ2y8",
-  authDomain: "eco-website-7f8e6.firebaseapp.com",
-  projectId: "eco-website-7f8e6",
-  storageBucket: "eco-website-7f8e6.appspot.com",
-  messagingSenderId: "829546577866",
-  appId: "1:829546577866:web:ed328bea7ebdcdc0bad933",
-  measurementId: "G-W8WPMYYPLF",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+
 
 // Initialize
 const app = initializeApp(firebaseConfig);
